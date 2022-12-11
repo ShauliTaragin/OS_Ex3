17e2e873125ae03d8befddd9bccb784884f12a38
Ido bar <75562486+idobar1403@users.noreply.github.com>
2022-12-11 20:09:30 +0200

    Add files via upload

--- reciver.c
+++ reciver.c
@@ -11,23 +11,132 @@
 #include <sys/time.h>
 #include <strings.h>
 #include <arpa/inet.h>
+
+#include <sys/un.h>
 #include <signal.h>
 
 #define SERVER_PORT 5090
 #define BUFFER_SIZE 1024
+
+#define SOCK_PATH "echo_sock"
 // function to calculate the checksum
 static int sum = 0;
 void checksum(char * buffer, int n)
 {
-    for (int i = 0; i<n; i++) {
-        sum += buffer[i];
-    }
+    // int sum =0 ;
+    // char buffer[BUFFER_SIZE];
+    // FILE *f = fopen(filepath, "r");
+    // if (f == NULL) {
+    //     perror("Error opening file");
+    //     return -1;
+    // }
+    // while (fgets(buffer, BUFFER_SIZE, f) != NULL) {
+    //     int i;
+        for (int i = 0; i<n; i++) {
+            sum += buffer[i];
+        }
+    
+    // fclose(f);
+    // return sum;
+}
 
+// int main(void)
+// {
+//     int s, s2, t, len;
+//     struct sockaddr_un local, remote;
+//     char str[100];
+//     if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+//         perror("socket");
+//         exit(1);
+//     }
+//     local.sun_family = AF_UNIX;
+//     strcpy(local.sun_path, SOCK_PATH);
+//     unlink(local.sun_path);
+//     len = strlen(local.sun_path) + sizeof(local.sun_family);
+//     if (bind(s, (struct sockaddr *)&local, len) == -1) {
+//         perror("bind");
+//         exit(1);
+//     }
+//     if (listen(s, 5) == -1) {
+//         perror("listen");
+//         exit(1);
+//     }
+//     for(;;) {
+//         int done, n;
+//         printf("Waiting for a connection...\n");
+//         t = sizeof(remote);
+//         if ((s2 = accept(s, (struct sockaddr *)&remote, &t)) == -1) {
+//             perror("accept");
+//             exit(1);
+//         }
+//         printf("Connected.\n");
+//         done = 0;
+//         do {
+//             n = recv(s2, str, 100, 0);
+//             if (n <= 0) {
+//                 if (n < 0) perror("recv");
+//                 done = 1;
+//             }
+//             if (!done) 
+//                 if (send(s2, str, n, 0) < 0) {
+//                     perror("send");
+//                     done = 1;
+//                 }
+//         } while (!done);
+//         close(s2);
+//     }
+//     return 0;
+// }
+
+
+void UDS_Stream_Socket(){
+    int  s2, t, len;
+    struct sockaddr_un sock_in,sock_out;
+    char buff[100];
+    int listeningSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (listeningSocket == -1) {
+        perror("socket");
+    }
+    sock_in.sun_family = AF_UNIX;
+    strcpy(sock_in.sun_path, SOCK_PATH);
+    unlink(sock_in.sun_path);
+    len = strlen(sock_in.sun_path) + sizeof(sock_in.sun_family);
+    if (bind(listeningSocket, (struct sockaddr *)&sock_in, len) == -1) {
+        perror("error in binding");
+        exit(1);
+    }
+    if (listen(listeningSocket, 10) == -1) {
+        perror("error in listening");
+        exit(1);
+    }
+     int done, n;
+        printf("Waiting for a connection...\n");
+        len = sizeof(sock_in);
+        int clientSocket= accept(listeningSocket, (struct sockaddr *)&sock_in,&len);
+        if (clientSocket == -1) {
+            perror("error in accepting");
+            exit(1);
+        }
+        printf("Connected.\n");
+    char checkSum[10];
+    recv(clientSocket, checkSum, 10, 0);
+    printf("got checksum ===>>> %s\n",checkSum);
+    int numOfBytes = -1;
+    while (numOfBytes!=0){
+        bzero(buff,sizeof(buff));
+        numOfBytes = recv(clientSocket, buff, sizeof(buff), 0);
+        checksum(buff,100);
+        // printf("%d",numOfBytes);
+    }
+    printf("checksum ====> %d", sum);
+    close(clientSocket);
+    close(listeningSocket);
 }
 
 void TCP(){
     char buffer[100];
-    //  open a new listening socket.
+    // printf("%d\n",checksum("100mb.txt",0));
+    //  1. open a new listening socket.
     int listeningSocket = -1;
     listeningSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (listeningSocket == -1)
@@ -35,7 +144,7 @@ void TCP(){
         perror("Error: Cannot open a new socket\n");
         return ;
     }
-    //  Listening to incoming connections.
+    //  2. Listening to incoming connections.
     struct sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_port = htons(SERVER_PORT);
@@ -58,7 +167,7 @@ void TCP(){
         double time = 0.0;
         memset(&clientAddress, 0, sizeof(clientAddress));
         clientAddressLen = sizeof(clientAddress);
-        // accept the socket from client.
+        // stage 3 - accept the socket from client.
         int clientSocket = accept(listeningSocket, (struct sockaddr *)&clientAddress, &clientAddressLen);
         if (clientSocket == -1)
         {
@@ -69,45 +178,39 @@ void TCP(){
         char response[10] = "OK";
         write(clientSocket, response, sizeof(response));
         gettimeofday(&begin, NULL);
+        char checkSum[20];
+        // int checkSumSize = recv(clientSocket,checkSum,20,0);
+        printf("checksum received  ===>  %s\n",checkSum);
         
-
-        //receive checksum from sender
-        char sum_recieved_string[20];
-        bzero(sum_recieved_string,sizeof(sum_recieved_string));
-        int checkSumSize = recv(clientSocket,sum_recieved_string,20,0);
-        printf("checksum received  ===>  %s\n",sum_recieved_string);
-        
-        
-
         int numOfBytes = -1;
         FILE *file = fopen("receiver.txt","w");
         while (numOfBytes != 0)
         {
-            // recieve the message from the client.
+            // stage 4/7 - recieve the massege from the client.
             bzero(buffer,sizeof(buffer));
             numOfBytes = recv(clientSocket, buffer, 100, 0);
             char* buf = buffer;
             buffer[100]='\0';
             buffer[101]='\0';
+            // printf("%c",buffer[100]);
+            // printf("buffer ==> %s\n",buffer);
+            // break;
             checksum(buffer,100);
             fputs(buffer,file);
+            // printf("got => %s",buffer);
         }
+        // int checkSumAns = checksum("receiver.txt",0);
+        // char checkSumString[20];
+        // sprintf(checkSumString,"%d",checkSumAns);
         printf("checksum  calculated  ===>  %d\n",sum);
         gettimeofday(&end, NULL);
         double time_current = (double)((end.tv_sec - begin.tv_sec) * 1000000 + end.tv_usec - begin.tv_usec) / 1000000;
         time += time_current;
         buffer[numOfBytes] = '\0';
-        char sum_in_string [20];
-        sprintf(sum_in_string,"%d",sum);
-        //only if the checksum is correct then we print the 
-        if(strcmp(sum_in_string,sum_recieved_string)==0){
-            printf("time took %f\n",time_current);
-        }
-        else{
-            printf("%d" , -1);
-        }
+        printf("time took %f\n",time_current);
+
         sleep(1);
-    // close connection.
+    // stage 10 - close connection.
     close(listeningSocket);
     fclose(file);
 }
@@ -115,6 +218,9 @@ int main(int argc, char **argv)
 {
     if(strcmp(argv[1],"a")==0){//case for tcp
         TCP();
+    }
+    if(strcmp(argv[1],"b")==0){//case for tcp
+        UDS_Stream_Socket();
     }
     return 1;
 }
--- sender.c
+++ sender.c
@@ -11,10 +11,15 @@
 #include <sys/time.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
 
 #define SERVER_PORT 5090
 #define BUFFER_SIZE 1024
 
+#define SOCK_PATH "echo_sock"
 int checksum(char * filepath, int n)
 {
     int sum =0 ;
@@ -33,21 +38,78 @@ int checksum(char * filepath, int n)
     fclose(f);
     return sum;
 }
+
+
+void UDS_Stream_Socket(){
+    int length;
+    char *fileName = "shauli.txt";
+    FILE *file;
+    char buf[256];
+    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(sockfd == -1){
+        perror("failed to open socket");
+    }
+
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    strcpy(addr.sun_path, SOCK_PATH);
+    length = strlen(addr.sun_path) + sizeof(addr.sun_family);
+    int Connect = connect(sockfd, (struct sockaddr*)&addr, length);
+    if (Connect == -1)
+    {
+        perror("error in connect to usd");
+        return ;
+    }
+
+    file = fopen(fileName, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error in opening file");
+        return;
+    }
+    char getReply[10];
+    // bzero(getReply, sizeof(getReply));
+    // read(sockfd, getReply, sizeof(getReply));
+    char sendbuffer[100];
+    int checkSumAns = checksum(fileName,0);
+    char checkSum[10];
+    sprintf(checkSum,"%d",checkSumAns);
+    send(sockfd, checkSum, 10, 0);
+    printf("checkSum Calculated ===>   %s\n",checkSum);
+    // int SendByte = send(senderSocket,checkSum,20,0);
+    int b;
+    int sum = 0;
+    do
+    {
+        bzero(sendbuffer,sizeof(sendbuffer));
+        b = fread(sendbuffer, 1, sizeof(sendbuffer), file);
+        // printf("%s\n",sendbuffer);
+        int SendByte = send(sockfd, sendbuffer, sizeof(sendbuffer), 0);
+        // printf("%d\n",SendByte);
+        sum = SendByte + sum;
+    } while (!feof(file));
+   
+    sleep(1);
+    //6. Close connection.
+    fclose(file);
+    close(sockfd);
+
+    sleep(1);
+}
 void TCP(){
-    //prepare file to sent to receiver
     char *fileName = "shauli.txt";
     FILE *file;
     char buf[256];
     socklen_t length;
 
-    //  open tcp socket.
+    // stage 1 - open tcp socket.
     int senderSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (senderSocket == -1)
     {
         perror("failed to open socket");
     }
     length = sizeof(buf);
-    //  create connection with measure.
+    // stage 2 - create connection with measure.
     struct sockaddr_in serverAddress;
     memset(&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
@@ -59,7 +121,7 @@ void TCP(){
         perror("error");
         return ;
     }
-    //  send file.
+    // stage 3/5 - send file.
     file = fopen(fileName, "r");
     if (file == NULL)
     {
@@ -73,22 +135,24 @@ void TCP(){
     int checkSumAns = checksum(fileName,0);
     char checkSum[20];
     sprintf(checkSum,"%d",checkSumAns);
+    send(senderSocket,checkSum,strlen(checkSum),0);
     printf("checkSum Calculated ===>   %s\n",checkSum);
-    int SendByte = send(senderSocket,checkSum,20,0);
+    // int SendByte = send(senderSocket,checkSum,20,0);
     int b;
     int sum = 0;
     do
     {
+        printf("entered");
         bzero(sendbuffer,sizeof(sendbuffer));
         b = fread(sendbuffer, 1, sizeof(sendbuffer), file);
-        int SendByte = send(senderSocket, sendbuffer, b, 0);
+        int SendByte = send(senderSocket, sendbuffer, strlen(sendbuffer), 0);
         sum = SendByte + sum;
     } while (!feof(file));
    
     sleep(1);
-    //Close connection.
+    //6. Close connection.
     close(senderSocket);
-    fclose(file);
+
     sleep(1);
 }
 
@@ -96,6 +160,9 @@ int main(int argc, char **argv)
 {
     if(strcmp(argv[1],"a")==0){//case for tcp
         TCP();
+    }
+    if(strcmp(argv[1],"b")==0){//case for tcp
+        UDS_Stream_Socket();
     }
     return 1;
 }
